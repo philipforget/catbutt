275b34c0343674d0052292180223e569503bd6ad
Philip Forget <philipforget@gmail.com>
2020-03-26 13:15:26 -0400

    barebones setup.py just for fun

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='catbutt',
+      version='0.1',
+      description='An example repo using cats',
+      url='http://github.com/philipforget/catbutts',
+      author='Philip Forget',
+      author_email='philipforget@gmail.com',
+      license='WTFPL',
+      packages=['catbutt'],
+      zip_safe=True)
